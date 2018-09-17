3f8923730f8ae8203edb8491a324f05c5724a368
ShidongS <43020747+ShidongS@users.noreply.github.com>
2018-09-17 14:41:41 -0400

    Add files via upload
    This is a linux bash script which can be regarded as a starting program.

---
+++ link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GOOGLE_APPLICATION_CREDENTIALS="/home/gary/Downloads/My Project API.json"
+python3 gary2.py
+python3 google.py
+ffmpeg -r 1 -f image2 -s 1080*720 -i /home/gary/EC601/picture2/%04d.jpg -pix_fmt yuv420p /home/gary/EC601/test.mp4
+
+
+
+
+
