9cc3ec039ec992f900ac79281f0435b4214cd8db
ShidongS <43020747+ShidongS@users.noreply.github.com>
2018-09-18 23:49:28 -0400

    Add files via upload

--- google.py
+++ google.py
@@ -1,21 +1,5 @@
-
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-def run_quickstart():
-    # [START vision_quickstart]
+# Author - Shidong Sun
+def google_vision():
     import io
     import os
     import PIL
@@ -24,45 +8,51 @@ def run_quickstart():
     from PIL import ImageDraw
 
     # Imports the Google Cloud client library
-    # [START vision_python_migration_import]
     from google.cloud import vision
     from google.cloud.vision import types
-    # [END vision_python_migration_import]
-
+   
     # Instantiates a client
-    # [START vision_python_migration_client]
     client = vision.ImageAnnotatorClient()
-    # [END vision_python_migration_client]
 
-    # The name of the image file to annotate
     i=0
-    for root,dir,files in os.walk('/home/gary/EC601/picture/'):
-        i+=len(files)
+    # Scan all files in the folder where the images from Twitter are stored
+    folder=os.getcwd()+'/picture/'
+    for root,dirname,filenames in os.walk(folder):  
+        for filename in filenames:  
+            if os.path.splitext(filename)[1]=='.jpg':
+                i += 1
     print(i)
-    for k in range(1,i+1):
-        file_name = os.path.join(os.path.dirname(__file__),'/home/gary/EC601/picture/'+'%04d'%(k)+'.jpg')
 
-    # Loads the image into memory
+    # Keep sending images to google vision until there is no file left
+    for k in range(1,i+1):
+        file_name = os.path.join(os.path.dirname(__file__),folder+'%04d'%(k)+'.jpg')
+        # Load the image into memory
         with io.open(file_name, 'rb') as image_file:
             content = image_file.read()
-
         image = types.Image(content=content)
-
-    # Performs label detection on the image file
+        # Run label detection on the image file
         response = client.label_detection(image=image)
         labels = response.label_annotations
         print('Labels:')
-        for label in labels:
-            print(label.description)
-            tag=label.description
-        im1=Image.open('/home/gary/EC601/picture/'+'%04d'%(k)+'.jpg')
-        font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf",40)
-        draw = ImageDraw.Draw(im1)
-        draw.text((0, 0),tag,(255,255,0),font=font)
-        draw = ImageDraw.Draw(im1)
-        im1.save('/home/gary/EC601/picture2/'+'%04d'%(k)+'.jpg')
-    # [END vision_quickstart]
 
+        tag=[]
+        # Save the all labels with the sequence of their possibilities
+        for label in labels:
+            tag.append(label.description)
+        print(tag)
+        l=len(tag)
+        # Modify the imags and add labels on them
+        folder2=os.getcwd()+'/picture2/'
+        if not os.path.exists(folder2):
+            os.makedirs(folder2)
+        img=Image.open(folder+'%04d'%(k)+'.jpg')
+        img=img.resize((720,720),resample=Image.LANCZOS)
+        font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf",40)
+        draw = ImageDraw.Draw(img)
+        for m in range(0,l):
+            draw.text((0, 40*m),tag[m],(255,0,0),font=font)
+        draw = ImageDraw.Draw(img)
+        img.save(folder2+'%04d'%(k)+'.jpg')
 
 if __name__ == '__main__':
-    run_quickstart()
+    google_vision()
